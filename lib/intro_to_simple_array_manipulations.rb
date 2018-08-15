31c883464cb727672088160de7e6ef8ec4edf9ef
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 22:54:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,10 @@ def using_push(countries_in_western_africa, next_country)
   next_country = "Niger"
   countries_in_western_africa.push(next_country)
 end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  new_neighborhood = "Brooklyn Heights"
+  neighborhoods_in_northwest_brooklyn.push(new_neighborhood)
+end
+  
