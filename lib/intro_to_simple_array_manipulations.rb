f4aa6f71efc5ab336cf35592fbda4daa783fc911
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 23:36:41 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ end
  def using_concat(my_favorite_things, more_favs)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   @more_favs = ["mario kart", "flatiron school"]
-  all_my_favs = my_favorite_things.concat(more_favs)
+  all_my_favs = @my_favorite_things.concat(@more_favs)
 end
  def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
