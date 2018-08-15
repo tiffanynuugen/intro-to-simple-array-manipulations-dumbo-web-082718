ce605b25bad574b06e52167a517ff29a36158bc4
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 23:21:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,6 +12,7 @@ end
 
 def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+end
   great_hits_of_the_nineties.pop
 end
 
