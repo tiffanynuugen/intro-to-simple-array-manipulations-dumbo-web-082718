bea79a9c1e3d92be11549e694f9cf438a787b0b7
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 23:03:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,24 +22,24 @@ end
 
 def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = @my_favorite_cities.shift
+  im_so_over_this_city = my_favorite_cities.shift
 end
 
 def shift_with_args(ice_cream_brands)
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  brands_removed = @ice_cream_brands.shift(2)
+  brands_removed = ice_cream_brands.shift(2)
 end
 
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["mario kart", "flatiron school"]
-  all_my_favs = @my_favorite_things.concat(more_favs)
+  all_my_favs = my_favorite_things.concat(more_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
   another_esoteric_language = "Malbolge"
-  new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
+  new_array = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
 end
 
 def using_uniq(captain_planet_and_the_planeteers)
