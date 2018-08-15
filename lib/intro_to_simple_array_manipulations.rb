c622637431bdfec73dd5a6afd3c8174ba70f3a73
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 23:31:09 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,7 @@ def using_pop(great_hits_of_the_nineties)
   @great_hits_of_the_nineties.pop
 end
  def pop_with_args(chars_in_game_of_thrones)
-  @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+  @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
   @chars_in_game_of_thrones.pop(2)
 end
  def using_shift(my_favorite_cities)
