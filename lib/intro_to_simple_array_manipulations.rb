3d8c4294203bec1de03c8579f8fb87ea23f8cd48
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 22:56:06 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,5 +7,5 @@ end
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.push(new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
