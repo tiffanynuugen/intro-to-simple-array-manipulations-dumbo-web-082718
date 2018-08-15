f714d5aa720efef3193e564691ff662fe2b4003a
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 23:18:07 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,7 +48,7 @@ def using_uniq(captain_planet_and_the_planeteers)
 end
 
 def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  private_colleges_in_newyork = ["New York University", "Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
   flat_array = private_colleges_in_newyork.flatten
 end
 
