a2cb74a990ff60945906b42a265b460755b096bd
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 22:35:48 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger".push
-  updated_array = using_push(countries_in_western_africa, next_country)
+  next_country = "Niger"
+  next_country.push(next_country) 
 end
